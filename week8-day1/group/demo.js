cc6688a7cc346003983a560cf4ca6ff00b074c68
JasonLei <jlei@activision.com>
2023-10-17 21:32:01 -0700

    added group challenge

---
+++ week8-day1/group/demo.js
@@ -0,0 +1,18 @@
+//USE THE FOLLOWING INFORMATION IN YOUR CODE
+//Image URL: https://cdn.vox-cdn.com/thumbor/lwffO18WrM9ujCXVqrFBg9JH200=/1400x1400/filters:format(png)/cdn.vox-cdn.com/uploads/chorus_asset/file/13354209/avatar.png
+//Plot: Set several years after the events of Avatar, Sully is now chieftain and Neytiri high priestess of their clan – although events will apparently focus on their children.
+
+//Write your VUE JS code below:
+Vue.component('mheading', {
+    template: '<h1>Hello, welcome to VUE Components!</h1>'
+})
+
+
+var vm1 = new Vue({
+    el: 'mheading'
+});
+
+var vm2 = new Vue({
+    el: 'mcontent'
+});
+
